db16f79f036251a6bd8d2cd6d750e41776294d08
Msc <msc@Mscs-MacBook-Pro.local>
2016-07-04 19:45:36 +0800

    Initial commit

---
+++ application/index/controller/Index.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return '学习ThinkPHP';
+    }
+
+    public function hellos()
+    {
+        return 'hello';
+    }
+
+    public function hello($name = 'World')
+    {
+        return 'Hello,' . $name . '！';
+    }
+    public function say(){
+        return 'speak php';
+    }
+}
---
+++ application/route.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+// $Id$
+//use think\Route;
+
+//Route::rule('hello/:name', 'index/hello');
+return [
+    '__pattern__' => [
+        'name' => '\w+',
+    ],
+    '[hello]'     => [
+        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+        ':name' => ['index/hello', ['method' => 'post']],
+    ],
+    // 路由参数name为可选
+    //'hello/[:name]' => 'index/hellos',
+    //'hellos' => 'index/hellos'
+];
